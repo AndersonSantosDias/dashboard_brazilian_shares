b366cc8a08a5b76b8b1f4d92babe7dac31e95a66
AndersonSantosDias <andersonds.dev.com>
2025-10-18 11:49:38 -0300

    fix: altura fixa

--- src/components/RankItem.jsx
+++ src/components/RankItem.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 const RankItem = ({marketCap, Change, logo, tiker, longName}) => {
   const isPositive = parseFloat(Change) > 0; 
   return (
-    <Link to={`/share/${tiker}`} className="flex p-4 w-full justify-between items-center transition-colors hover:bg-[rgb(var(--color-bg))]">
+    <Link to={`/share/${tiker}`} className="flex p-4 w-full h-18 justify-between items-center transition-colors hover:bg-[rgb(var(--color-bg))]">
       <div className="flex gap-2 items-center">
       <img src={logo} alt={tiker} className="w-8 h-8 rounded-full bg-white"/>
       <div>
